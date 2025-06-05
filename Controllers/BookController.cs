963ea8d6dc138697f9920969f7906662a58d485a
Ronohayon27 <ronohayon27@gmail.com>
2025-06-05 13:59:42 +0300

    Adding Controller, Get All, Get book by ID, createBook

---
+++ Controllers/BookController.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BookApi.Data;
+using BookApi.Models;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BooksController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        // GET: api/books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return book;
+        }
+        // POST: api/books
+        [HttpPost]
+        public async Task<ActionResult<Book>> CreateBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetBook), new { id = book.Id }, book);
+        }
+
+    }
+}
--- Models/Book.cs
+++ Models/Book.cs
@@ -3,21 +3,28 @@ using System.Xml.Serialization;
 
 namespace BookApi.Models
 {
+    // Book model,
     public class Book
     {
+        // identifier
         public int Id { get; set; }
 
+
+        // Name of the book
         [Required]
         [StringLength(100)]
         public string Title { get; set; } = string.Empty;
 
+        // Name of the Author
         [Required]
         [StringLength(50)]
         public string Author { get; set; } = string.Empty;
-
+        
+        // Date of publication
         [DataType(DataType.Date)]
         public DateTime PublicationDate { get; set; }
-
+        
+        // Book Price
         [Range(0.01, 10000.00)]
         public decimal Price { get; set; }
     }
